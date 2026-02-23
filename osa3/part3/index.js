a3ab749294c72e20a8eeba3045e0c2195a24548a
paajooni <paajooni@student.jyu.fi>
2026-02-23 20:33:31 +0200

    tehtävät 3.7 ja 3.8

--- osa3/part3/index.js
+++ osa3/part3/index.js
@@ -21,6 +21,17 @@ let notes = [
   }
 ]
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World! :-) </h1>')
 })
@@ -47,13 +58,27 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.post('/api/notes', (request, response) => {
+const generateId = () => {
   const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id))) 
+    ? Math.max(...notes.map(n => Number(n.id)))
     : 0
+  return String(maxId + 1)
+}
 
-  const note = request.body
-  note.id = String(maxId + 1)
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
 
   notes = notes.concat(note)
 
@@ -69,3 +94,10 @@ app.listen(PORT, () => {
 app.get('/favicon.ico', (request, response) => {
   response.status(204).end()
 })
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
