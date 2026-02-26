d939be034c6cffb1143675453c9834151c7a3cdd
paajooni <paajooni@student.jyu.fi>
2026-02-26 19:29:13 +0200

    Tehtävä 3.9 ja 3.10

--- osa3/puhelinluetteloBackend/index.js
+++ osa3/puhelinluetteloBackend/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static(('dist')))
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body)
@@ -102,7 +103,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
