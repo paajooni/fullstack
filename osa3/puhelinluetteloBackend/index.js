4dc40038a0ebc1c6c7ee21331e0ff90d9a6bf532
paajooni <paajooni@student.jyu.fi>
2026-03-14 16:17:29 +0200

    Tehtävään 3.18 asti tehty

--- osa3/puhelinluetteloBackend/index.js
+++ osa3/puhelinluetteloBackend/index.js
@@ -22,14 +22,19 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 let persons = []
 
-app.get('/info', (request, response)=> {
+app.get('/info', (request, response, next)=> {
     const date = new Date()
-    response.send(
-        `<div>
-            <p>Phonebook has info for ${persons.length} people</p>
+    Person.countDocuments({})
+      .then(count => {
+        const info = `
+        <div>
+            <p>Phonebook has info for ${count} people</p>
             <p>${date}</p>
-        </div>`
-    )
+        </div>
+        `
+        response.send(info)
+        })
+        .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
@@ -55,10 +60,10 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-  .then(result => {
-  response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -94,13 +99,13 @@ app.put('/api/persons/:id', (request, response, next) => {
   const { name, number } = request.body
 
   Person.findById(request.params.id)
-  .then(note => {
+  .then(person => {
     if (!person) {
       return response.status(404).end()
     }
 
-    note.name = name
-    note.number = number
+    person.name = name
+    person.number = number
     
     return person.save().then((updatedPerson) => {
       response.json(updatedPerson)
