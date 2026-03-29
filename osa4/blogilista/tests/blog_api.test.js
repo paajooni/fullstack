e18065362e3e541b2a24543de65571b97db5699a
paajooni <paajooni@student.jyu.fi>
2026-03-30 00:33:12 +0300

    Edistystä testaukseen

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,9 +1,35 @@
-const { test, after } = require('node:test')
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    "title" : "fullstack is okay",
+    "author" : "Jonne Kiiskinen",
+    "url" : "yle.fi/fullstack/okay",
+    "likes" : 68
+  },
+  {
+    "title" : "I hate all stacks",
+    "author" : "Veikka Kumpu",
+    "url" : "yle.fi/fullstack/hate",
+    "likes" : 1
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
 
 test('blogs are returned as json', async () => {
     await api
@@ -11,6 +37,20 @@ test('blogs are returned as json', async () => {
         .expect(200)
         .expect('Content-type', /application\/json/)
 })
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('a specific blog is withing the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(e => e.title)
+    assert(contents.includes('fullstack is okay'))
+})
+
 
 after(async () => {
     await mongoose.connection.close()
